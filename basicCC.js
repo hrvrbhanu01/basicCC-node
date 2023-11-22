809b22c7c114533d8086c2ff9988f555613d337f
Bhanu Prakash Dixit <74755575+hrvrbhanu01@users.noreply.github.com>
2023-11-22 18:29:11 +0530

    bcc

---
+++ basicCC.js
@@ -0,0 +1,29 @@
+const {Contract}=require('fabric-contract-api');
+
+class testContract extends Contract{
+    //functions goes here
+
+    //creating an object to store marks of a student in each subject and storing this obj as a value and key as studentId
+    async addMarks(ctx, studentId, subject1, subject2, subject3){
+        let marks={
+            subj1:subject1,
+            subj2:subject2,
+            subj3:subject3
+        };
+        await ctx.stub.putState(studentId, Buffer.from(JSON.stringify(marks)));
+
+        console.log('Student Marks added to the ledger Successfully...');
+    }
+
+    //Delete Marks
+    async deleteMarks(ctx, studentId){
+        await ctx.stub.deleteState(studentId);
+
+        console.log('Student Marks deleted from ledger Successfully...');
+    }
+
+    //Query student marks
+    async queryMarks(ctx, studentId){
+        let marksAsBytes
+    }
+}
